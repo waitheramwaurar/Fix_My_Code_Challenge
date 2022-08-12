9e72f5702ed5973f26d437c7c80f4e56f7913bb5
Waithera Mwaura <rwaitheramwaura@gmail.com>
2022-08-12 13:04:02 +0000

    Right square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -17,7 +17,7 @@ if (process.argv.length <= 2) {
 size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < i ; j ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
